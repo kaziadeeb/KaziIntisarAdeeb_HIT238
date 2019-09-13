37e685add7e54a35fa47709aa38d44c24edaf89e
kaziadeeb <kazi.intisar.adeeb96@gmail.com>
2019-09-13 15:21:28 +0930

    some tweaks

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
-var blueMountainLat=-33.6176;
-var blueMountainLong=150.4559;
+var mapElement = document.getElementById("mdisplay");
+var latitude=parseFloat(mapElement.dataset.lat);
+var longitude=parseFloat(mapElement.dataset.long);
 var mapDisplay = "mdisplay";
 var map;
 
 function createMap(){
     map = new L.Map(mapDisplay, {
-        center: new L.LatLng(blueMountainLat, blueMountainLong),      
+        center: new L.LatLng(latitude, longitude),      
         zoom: 14,      
         layers: new L.TileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png")  
     }); 
 
-    var marker = new L.Marker(new L.LatLng(blueMountainLat, blueMountainLong));
-    marker.bindPopup("Blue Mountains");
+    var marker = new L.Marker(new L.LatLng(latitude, longitude));
+    marker.bindPopup(mapElement.dataset.site);
     map.addLayer(marker);
 }
 
