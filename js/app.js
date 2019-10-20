22b70b33cc87244d170fded564c8784010acd579
kaziadeeb <kazi.intisar.adeeb96@gmail.com>
2019-10-20 17:22:24 +0930

    Added javascript code for the homepage slides

--- js/app.js
+++ js/app.js
@@ -6,10 +6,10 @@ var map;
 
 function createMap(){
     map = new L.Map(mapDisplay, {
-        center: new L.LatLng(latitude, longitude),      
-        zoom: 14,      
-        layers: new L.TileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png")  
-    }); 
+        center: new L.LatLng(latitude, longitude),
+        zoom: 14,
+        layers: new L.TileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png")
+    });
 
     var marker = new L.Marker(new L.LatLng(latitude, longitude));
     marker.bindPopup(mapElement.dataset.site);
@@ -17,3 +17,32 @@ function createMap(){
 }
 
 createMap();
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+var i;
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("dot");
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+}
+for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
+}
